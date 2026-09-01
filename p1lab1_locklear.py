e1356df78f0637596221f9854bc4372ffea26711
Locklear878 <lockleac2222@student.faytechcc.edu>
2026-09-01 18:39:55 +0000

    p1lab1 finished

--- p1lab1_locklear.py
+++ p1lab1_locklear.py
@@ -6,3 +6,15 @@
 
 # First, we're going to greet the user
 print("Hello!")
+
+# Next, ask their first name
+#print("What's your name?")
+first_name = input("What's your first name? ")
+
+# do the same thing for last_name
+last_name = input("What's your last name? ")
+
+# They say back the name
+print("Nice to meet you", first_name, last_name)
+
+
